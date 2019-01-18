95e59757f95d6bf0186349f989b3ead20ba16a6c
Ferit Tunçer <ferit@cryptolab.net>
2019-01-18 15:59:12 +0300

    feat(event-handler): centralized arbitrator dashboard

---
+++ src/event-handlers/centralized-arbitrator-dashboard.js
@@ -0,0 +1,129 @@
+const centralizedArbitrator = require('@kleros/kleros-interaction/build/contracts/CentralizedArbitrator.json')
+
+const _web3 = require('../utils/web3')
+const _sendgrid = require('../utils/sendgrid')
+const dynamoDB = require('../utils/dynamo-db')
+
+module.exports.post = async (
+  centralizedArbitratorAddress,
+  event,
+  _context,
+  callback
+) => {
+  // Get the event body
+  const body = JSON.parse(event.body)
+  if (body == null) {
+    return callback(null, {
+      statusCode: 400,
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      body: JSON.stringify({
+        error: 'No event log passed in body.'
+      })
+    })
+  }
+
+  const centralizedArbitratorInstance = new _web3.eth.Contract(
+    centralizedArbitrator.abi,
+    centralizedArbitratorAddress,
+    {
+      gasPrice: 20000000000
+    }
+  )
+
+  const owner = centralizedArbitratorInstance.methods.owner().call()
+
+  // Fetch from the user-settings table
+  const item = await dynamoDB.getItem({
+    Key: { address: { S: owner } },
+    TableName: 'user-settings',
+    AttributesToGet: ['email']
+  })
+
+  const item2 = await dynamoDB.getItem({
+    Key: { address: { S: owner } },
+    TableName: 'user-settings',
+    AttributesToGet: [
+      'centralizedArbitratorDashboardNotificationSettingDisputes'
+    ]
+  })
+
+  const item3 = await dynamoDB.getItem({
+    Key: { address: { S: owner } },
+    TableName: 'user-settings',
+    AttributesToGet: ['fullName']
+  })
+
+  let emailAddress
+  if (item && item.Item && item.Item.email) emailAddress = item.Item.email.S
+
+  let wantsEmailForDisputes
+  if (
+    item2 &&
+    item2.Item &&
+    item2.Item.centralizedArbitratorDashboardNotificationSettingDisputes
+  ) {
+    wantsEmailForDisputes =
+      item2.Item.centralizedArbitratorDashboardNotificationSettingDisputes.S
+  }
+
+  if (!wantsEmailForDisputes)
+    return callback(null, {
+      statusCode: 200,
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      body: JSON.stringify({
+        sent: false,
+        reason: "User doesn't want to receive emails for new disputes."
+      })
+    })
+
+  let fullName
+  if (item3 && item3.Item && item3.Item.fullName)
+    fullName = item3.Item.fullName.S
+
+  if (emailAddress == null)
+    return callback(null, {
+      statusCode: 200,
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      body: JSON.stringify({
+        sent: false,
+        reason: 'No email address found.'
+      })
+    })
+
+  // TODO Add your email template and message here
+  const msg = {
+    to: emailAddress,
+    from: {
+      name: 'Kleros',
+      email: 'contact@kleros.io'
+    },
+    templateId: 'd-8e6dd684d23447a8a5051adf69396d58',
+    dynamic_template_data: {
+      name: fullName,
+      arbitratorAddress: centralizedArbitratorAddress,
+      disputeID: body._disputeID,
+      subject: 'You Have A New Dispute Awaiting Your Arbitration',
+      eventName: body.event
+    }
+  }
+
+  // Sendgrid
+  const sendGridClient = await _sendgrid()
+  let sent = true
+  let reason
+  try {
+    await sendGridClient.send(msg)
+  } catch (err) {
+    sent = false
+    reason = err
+  }
+
+  callback(null, {
+    statusCode: 200,
+    headers: { 'Access-Control-Allow-Origin': '*' },
+    body: JSON.stringify({
+      sent,
+      reason
+    })
+  })
+}
