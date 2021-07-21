9cf714bbaa13fcdddb9bc779139cf582885eeaca
Henrique Barcelos <hbarcelos909@gmail.com>
2021-07-21 10:57:03 -0300

    chore: introduced eslint + prettier and lint-staged + husky

--- src/event-handlers/court-emails.js
+++ src/event-handlers/court-emails.js
@@ -1,206 +1,170 @@
-const qs = require('querystring');
-const { inspect } = require('util');
+const qs = require("querystring");
+const { inspect } = require("util");
 
-const TimeAgo = require('javascript-time-ago');
-TimeAgo.addLocale(require('javascript-time-ago/locale/en'));
+const TimeAgo = require("javascript-time-ago");
+TimeAgo.addLocale(require("javascript-time-ago/locale/en"));
 
-const _web3 = require('../utils/web3');
-const _sendgrid = require('../utils/sendgrid');
-const _klerosLiquid = require('../assets/contracts/KlerosLiquid.json');
-const _xKlerosLiquid = require('../assets/contracts/xKlerosLiquid.json');
-const dynamoDB = require('../utils/dynamo-db');
-const getEnvVars = require('../utils/get-env-vars');
-const webpush = require('web-push');
+const _web3 = require("../utils/web3");
+const _sendgrid = require("../utils/sendgrid");
+const _klerosLiquid = require("../assets/contracts/KlerosLiquid.json");
+const _xKlerosLiquid = require("../assets/contracts/xKlerosLiquid.json");
+const dynamoDB = require("../utils/dynamo-db");
+const getEnvVars = require("../utils/get-env-vars");
+const webpush = require("web-push");
 
 inspect.defaultOptions.depth = 5;
 
-const timeAgo = new TimeAgo('en-US');
+const timeAgo = new TimeAgo("en-US");
 
 const chainIdToCurrencySymbols = {
   1: {
-    native: 'ETH',
-    PNK: 'PNK'
+    native: "ETH",
+    PNK: "PNK",
   },
   100: {
-    native: 'xDAI',
-    PNK: 'stPNK'
-  }
+    native: "xDAI",
+    PNK: "stPNK",
+  },
 };
 
-const getSymbols = chainId => {
+const getSymbols = (chainId) => {
   return chainIdToCurrencySymbols[chainId] || chainIdToCurrencySymbols[1];
 };
 
-const createEventHandlers = chainId => {
+const createEventHandlers = (chainId) => {
   const symbols = getSymbols(chainId);
 
   return {
     Draw: async (_, klerosLiquid, event) => {
-      const dispute = await klerosLiquid.methods
-        .disputes(event._disputeID)
-        .call();
+      const dispute = await klerosLiquid.methods.disputes(event._disputeID).call();
 
       return [
         {
           account: event._address,
-          type: 'Draw',
+          type: "Draw",
           disputeID: event._disputeID,
-          templateId: 'd-b4880ab92d004827929ad074a714a7cb',
+          templateId: "d-b4880ab92d004827929ad074a714a7cb",
           dynamic_template_data: {
             startDate:
-              event._appeal === '0'
+              event._appeal === "0"
                 ? `${timeAgo.format(
                     (Number(dispute.lastPeriodChange) +
-                      Number(
-                        (
-                          await klerosLiquid.methods
-                            .getSubcourt(dispute.subcourtID)
-                            .call()
-                        ).timesPerPeriod[0]
-                      )) *
+                      Number((await klerosLiquid.methods.getSubcourt(dispute.subcourtID).call()).timesPerPeriod[0])) *
                       1000
                   )}`
-                : 'as soon as all other jurors are drawn',
+                : "as soon as all other jurors are drawn",
             caseNumber: event._disputeID,
-            caseUrl: `https://court.kleros.io/cases/${
-              event._disputeID
-            }?${qs.stringify({ requiredChainId: chainId })}`
+            caseUrl: `https://court.kleros.io/cases/${event._disputeID}?${qs.stringify({ requiredChainId: chainId })}`,
           },
-          pushNotificationText: `You have been drawn on case #${event._disputeID}`
-        }
+          pushNotificationText: `You have been drawn on case #${event._disputeID}`,
+        },
       ];
     },
     Vote: async (_, klerosLiquid, event) => {
-      const dispute = await klerosLiquid.methods
-        .disputes(event._disputeID)
-        .call();
+      const dispute = await klerosLiquid.methods.disputes(event._disputeID).call();
       return [
         {
           account: event._address,
-          type: 'Draw', // Use the same setting for Draw and Vote reminders
+          type: "Draw", // Use the same setting for Draw and Vote reminders
           disputeID: event._disputeID,
-          templateId: 'd-c3bfae61a6cc42c1ab744a10dad0eca7',
+          templateId: "d-c3bfae61a6cc42c1ab744a10dad0eca7",
           dynamic_template_data: {
             endTime: `${timeAgo.format(
               (Number(dispute.lastPeriodChange) +
-                Number(
-                  (
-                    await klerosLiquid.methods
-                      .getSubcourt(dispute.subcourtID)
-                      .call()
-                  ).timesPerPeriod[2]
-                )) *
+                Number((await klerosLiquid.methods.getSubcourt(dispute.subcourtID).call()).timesPerPeriod[2])) *
                 1000
             )}`,
             caseNumber: event._disputeID,
-            caseUrl: `https://court.kleros.io/cases/${
-              event._disputeID
-            }?${qs.stringify({ requiredChainId: chainId })}`
+            caseUrl: `https://court.kleros.io/cases/${event._disputeID}?${qs.stringify({ requiredChainId: chainId })}`,
           },
-          pushNotificationText: `It is time to vote on case #${event._disputeID}`
-        }
+          pushNotificationText: `It is time to vote on case #${event._disputeID}`,
+        },
       ];
     },
     VoteReminder: async (_, klerosLiquid, event) => {
-      const dispute = await klerosLiquid.methods
-        .disputes(event._disputeID)
-        .call();
+      const dispute = await klerosLiquid.methods.disputes(event._disputeID).call();
       return [
         {
           account: event._address,
-          type: 'Draw', // Use the same setting for Draw and Vote reminders
+          type: "Draw", // Use the same setting for Draw and Vote reminders
           disputeID: event._disputeID,
-          templateId: 'd-56da601bf4334c7c8fa2bd4c65777dca',
+          templateId: "d-56da601bf4334c7c8fa2bd4c65777dca",
           dynamic_template_data: {
             endTime: `${timeAgo.format(
               (Number(dispute.lastPeriodChange) +
-                Number(
-                  (
-                    await klerosLiquid.methods
-                      .getSubcourt(dispute.subcourtID)
-                      .call()
-                  ).timesPerPeriod[2]
-                )) *
+                Number((await klerosLiquid.methods.getSubcourt(dispute.subcourtID).call()).timesPerPeriod[2])) *
                 1000
             )}`,
             caseNumber: event._disputeID,
-            caseUrl: `https://court.kleros.io/cases/${
-              event._disputeID
-            }?${qs.stringify({ requiredChainId: chainId })}`
+            caseUrl: `https://court.kleros.io/cases/${event._disputeID}?${qs.stringify({ requiredChainId: chainId })}`,
           },
-          pushNotificationText: `You have 24 hours left to vote on case #${event._disputeID}`
-        }
+          pushNotificationText: `You have 24 hours left to vote on case #${event._disputeID}`,
+        },
       ];
     },
     Appeal: async (_, klerosLiquid, event) => {
       return [
         {
           account: event._address,
-          type: 'Appeal', // Use the same setting for Draw and Vote reminders
+          type: "Appeal", // Use the same setting for Draw and Vote reminders
           disputeID: event._disputeID,
-          templateId: 'd-2f9e67361add441b9a85b2116ba90e53',
+          templateId: "d-2f9e67361add441b9a85b2116ba90e53",
           dynamic_template_data: {
             caseNumber: event._disputeID,
-            caseUrl: `https://court.kleros.io/cases/${
-              event._disputeID
-            }?${qs.stringify({ requiredChainId: chainId })}`
+            caseUrl: `https://court.kleros.io/cases/${event._disputeID}?${qs.stringify({ requiredChainId: chainId })}`,
           },
-          pushNotificationText: `Case #${event._disputeID} has been appealed`
-        }
+          pushNotificationText: `Case #${event._disputeID} has been appealed`,
+        },
       ];
     },
     Won: async (_, klerosLiquid, event) => {
       return [
         {
           account: event._address,
-          type: 'Win', // Use the same setting for Draw and Vote reminders
+          type: "Win", // Use the same setting for Draw and Vote reminders
           disputeID: event._disputeID,
-          templateId: 'd-b6ac074fb68541b09e1463aa66e6aadb',
+          templateId: "d-b6ac074fb68541b09e1463aa66e6aadb",
           dynamic_template_data: {
             ethWon: `${event._ethWon} ${symbols.native}`,
             pnkWon: `${event._pnkWon} ${symbols.PNK}`,
             caseTitle: event._caseTitle,
             caseNumber: event._disputeID,
-            caseUrl: `https://court.kleros.io/cases/${
-              event._disputeID
-            }?${qs.stringify({ requiredChainId: chainId })}`
+            caseUrl: `https://court.kleros.io/cases/${event._disputeID}?${qs.stringify({ requiredChainId: chainId })}`,
           },
-          pushNotificationText: `Horray! You won ${event._ethWon} ${symbols.native} from case #${event._disputeID}`
-        }
+          pushNotificationText: `Horray! You won ${event._ethWon} ${symbols.native} from case #${event._disputeID}`,
+        },
       ];
     },
     Lost: async (_, klerosLiquid, event) => {
       return [
         {
           account: event._address,
-          type: 'Lose', // Use the same setting for Draw and Vote reminders
+          type: "Lose", // Use the same setting for Draw and Vote reminders
           disputeID: event._disputeID,
-          templateId: 'd-588ac6aef7184cad9f3c460dbef5433b',
+          templateId: "d-588ac6aef7184cad9f3c460dbef5433b",
           dynamic_template_data: {
             pnkLost: `${event._pnkLost} ${symbols.PNK}`,
             caseTitle: event._caseTitle,
             caseNumber: event._disputeID,
-            caseUrl: `https://court.kleros.io/cases/${
-              event._disputeID
-            }?${qs.stringify({ requiredChainId: chainId })}`
+            caseUrl: `https://court.kleros.io/cases/${event._disputeID}?${qs.stringify({ requiredChainId: chainId })}`,
           },
-          pushNotificationText: `You lost ${event._pnkLost} ${symbols.PNK} from case #${event._disputeID}`
-        }
+          pushNotificationText: `You lost ${event._pnkLost} ${symbols.PNK} from case #${event._disputeID}`,
+        },
       ];
     },
     StakeChanged: async (_, klerosLiquid, event) => {
       return [
         {
           account: event._address,
-          type: 'Stake',
-          templateId: 'd-231457425dfe444e99ff9f27db599f9c',
+          type: "Stake",
+          templateId: "d-231457425dfe444e99ff9f27db599f9c",
           dynamic_template_data: {
-            stakesChanged: event._stakesChanged
+            stakesChanged: event._stakesChanged,
           },
-          pushNotificationText: `Your court stakes have been updated`
-        }
+          pushNotificationText: `Your court stakes have been updated`,
+        },
       ];
-    }
+    },
   };
 };
 
@@ -210,60 +174,46 @@ const createLambdaHandler = (createWeb3, createContract) => {
     if (event === undefined || event === null) {
       return callback(null, {
         statusCode: 400,
-        headers: { 'Access-Control-Allow-Origin': '*' },
+        headers: { "Access-Control-Allow-Origin": "*" },
         body: JSON.stringify({
-          error: 'No event log passed in body.'
-        })
+          error: "No event log passed in body.",
+        }),
       });
     }
 
     const web3 = await createWeb3();
     const chainId = await web3.eth.getChainId();
     const sendgrid = await _sendgrid();
-    const { PRIVATE_KEY } = await getEnvVars(['PRIVATE_KEY']);
-    const lambdaAccount = web3.eth.accounts.privateKeyToAccount(
-      PRIVATE_KEY.replace(/^\s+|\s+$/g, '')
-    );
+    const { PRIVATE_KEY } = await getEnvVars(["PRIVATE_KEY"]);
+    const lambdaAccount = web3.eth.accounts.privateKeyToAccount(PRIVATE_KEY.replace(/^\s+|\s+$/g, ""));
 
     const handlers = createEventHandlers(chainId);
     let notifications = [];
     try {
-      notifications = await handlers[event.event](
-        web3,
-        createContract(web3),
-        event
-      );
+      notifications = await handlers[event.event](web3, createContract(web3), event);
     } catch (err) {
       const { event: eventName, ...payload } = event;
-      console.warn(
-        { event: eventName, payload, err },
-        'Error processing event'
-      );
+      console.warn({ event: eventName, payload, err }, "Error processing event");
 
       return callback(null, {
         statusCode: 500,
-        headers: { 'Access-Control-Allow-Origin': '*' },
+        headers: { "Access-Control-Allow-Origin": "*" },
         body: JSON.stringify({
-          error: err.message
-        })
+          error: err.message,
+        }),
       });
     }
 
     for (const notification of notifications) {
-      console.info({ notification }, 'Processing notification');
+      console.info({ notification }, "Processing notification");
       const signedUnsubscribeKey = lambdaAccount.sign(notification.account);
 
       try {
         const settingKey = `courtNotificationSetting${notification.type}`;
         const item = await dynamoDB.getItem({
           Key: { address: { S: notification.account } },
-          TableName: 'user-settings',
-          AttributesToGet: [
-            'email',
-            settingKey,
-            'pushNotifications',
-            'pushNotificationsData'
-          ]
+          TableName: "user-settings",
+          AttributesToGet: ["email", settingKey, "pushNotifications", "pushNotificationsData"],
         });
 
         let email;
@@ -274,85 +224,64 @@ const createLambdaHandler = (createWeb3, createContract) => {
         }
 
         if (email && setting) {
-          console.info(
-            { email, data: notification.dynamic_template_data },
-            'Sending email'
-          );
+          console.info({ email, data: notification.dynamic_template_data }, "Sending email");
 
           await sendgrid.send({
             to: email,
             from: {
-              name: 'Kleros',
-              email: 'noreply@kleros.io'
+              name: "Kleros",
+              email: "noreply@kleros.io",
             },
             templateId: notification.templateId,
             dynamic_template_data: {
               ...notification.dynamic_template_data,
-              unsubscribe: ` https://hgyxlve79a.execute-api.us-east-2.amazonaws.com/production/unsubscribe?signature=${signedUnsubscribeKey.signature}&account=${notification.account}&dapp=court`
-            }
+              unsubscribe: ` https://hgyxlve79a.execute-api.us-east-2.amazonaws.com/production/unsubscribe?signature=${signedUnsubscribeKey.signature}&account=${notification.account}&dapp=court`,
+            },
           });
         }
 
-        const pushNotifications =
-          item &&
-          item.Item &&
-          item.Item['pushNotifications'] &&
-          item.Item['pushNotifications'].BOOL;
+        const pushNotifications = item && item.Item && item.Item.pushNotifications && item.Item.pushNotifications.BOOL;
 
         let pushNotificationsData;
         try {
           pushNotificationsData =
-            item && item.Item && item.Item['pushNotificationsData']
-              ? JSON.parse(item.Item['pushNotificationsData'].S)
-              : null;
+            item && item.Item && item.Item.pushNotificationsData ? JSON.parse(item.Item.pushNotificationsData.S) : null;
         } catch (err) {
-          console.warn({ err }, 'Failed to parse push notifications data');
+          console.warn({ err }, "Failed to parse push notifications data");
           pushNotificationsData = null;
         }
 
         if (pushNotifications && pushNotificationsData) {
-          const { VAPID_KEY } = await getEnvVars(['VAPID_KEY']);
+          const { VAPID_KEY } = await getEnvVars(["VAPID_KEY"]);
           const options = {
             vapidDetails: {
-              subject: 'mailto:contact@kleros.io',
+              subject: "mailto:contact@kleros.io",
               publicKey: process.env.VAPID_PUB,
-              privateKey: VAPID_KEY
+              privateKey: VAPID_KEY,
             },
-            TTL: 60
+            TTL: 60,
           };
 
-          await webpush.sendNotification(
-            pushNotificationsData,
-            notification.pushNotificationText,
-            options
-          );
+          await webpush.sendNotification(pushNotificationsData, notification.pushNotificationText, options);
         }
       } catch (err) {
-        console.error({ notification, err }, 'Failed to process notification');
+        console.error({ notification, err }, "Failed to process notification");
       }
     }
 
     callback(null, {
       statusCode: 200,
-      headers: { 'Access-Control-Allow-Origin': '*' }
+      headers: { "Access-Control-Allow-Origin": "*" },
     });
   };
 };
 
 module.exports.post = createLambdaHandler(
   _web3,
-  web3 =>
-    new web3.eth.Contract(
-      _klerosLiquid.abi,
-      process.env.KLEROS_LIQUID_ADDRESS_MAINNET
-    )
+  (web3) => new web3.eth.Contract(_klerosLiquid.abi, process.env.KLEROS_LIQUID_ADDRESS_MAINNET)
 );
 
 module.exports.postXDai = createLambdaHandler(
   _web3.xdaiChain,
-  web3 =>
-    new web3.eth.Contract(
-      _xKlerosLiquid.abi,
-      process.env.X_KLEROS_LIQUID_ADDRESS_XDAI
-    )
+  (web3) => new web3.eth.Contract(_xKlerosLiquid.abi, process.env.X_KLEROS_LIQUID_ADDRESS_XDAI)
 );
--- src/utils/web3.js
+++ src/utils/web3.js
@@ -1,16 +1,16 @@
-const Web3 = require('web3')
+const Web3 = require("web3");
 
 module.exports = async () => {
-  const INFURA_URL = process.env.INFURA_URL
-  return new Web3(new Web3.providers.HttpProvider(INFURA_URL))
-}
+  const INFURA_URL = process.env.INFURA_URL;
+  return new Web3(new Web3.providers.HttpProvider(INFURA_URL));
+};
 
 module.exports.xdaiChain = async () => {
   try {
-    const XDAI_RPC_URL = process.env.XDAI_RPC_URL
-    return new Web3(new Web3.providers.HttpProvider(XDAI_RPC_URL))
+    const XDAI_RPC_URL = process.env.XDAI_RPC_URL;
+    return new Web3(new Web3.providers.HttpProvider(XDAI_RPC_URL));
   } catch (err) {
-    console.warn("Failed to create a web3 instance:", err)
-    throw err
+    console.warn("Failed to create a web3 instance:", err);
+    throw err;
   }
-}
+};
